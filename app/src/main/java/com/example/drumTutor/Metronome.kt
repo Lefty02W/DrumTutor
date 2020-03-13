5e0843d5ee467bfd96f5337719e261342ec74896
Luke Walsh <lwa383@uclive.ac.nz>
2020-03-13 18:07:59 +1300

    Basic metronome working with sound
    - when user changes temp in real time speed changes
    - label updates with tempo time

--- app/src/main/java/com/example/drumTutor/Metronome.kt
+++ app/src/main/java/com/example/drumTutor/Metronome.kt
@@ -1,18 +1,66 @@
 package com.example.drumTutor
 
+import android.media.AudioManager
+import android.media.ToneGenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_metronome.*
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class Metronome : AppCompatActivity() {
+    private var metronomeOn = false
+    private var metronome = Timer("metronome", true)
+    //  Initial BPM
+    private var bpmVal: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_metronome)
         croller.label = croller.progress.toString()
+        croller.setOnProgressChangedListener {
+            croller.label = croller.progress.toString()
+            if (this.metronomeOn && bpmVal != croller.progress.toLong()) {
+                stopMetronome()
+                startMetronome()
+            }
+            bpmVal = croller.progress.toLong()
 
-        croller.setOnProgressChangedListener { croller.label = croller.progress.toString() }
+        }
+    }
+
+    fun toggleMetronome(view: View) {
+        this.metronomeOn = !metronomeOn
+        var count = 0
+        if (this.metronomeOn) {
+            startMetronome()
+        } else {
+            stopMetronome()
+        }
+    }
+
+    private fun startMetronome() {
+        this.metronome.schedule(
+            timerTask {
+                val tone1 = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+                tone1.startTone(ToneGenerator.TONE_PROP_BEEP)
+                tone1.release()
+            }, 500L, calculatePause()
+        )
+    }
+
+    private fun stopMetronome() {
+        this.metronome.cancel()
+        newTimer()
+    }
+
+    private fun newTimer() {
+        this.metronome = Timer("metronome", true)
+    }
+
+    private fun calculatePause(): Long {
+        return (1000.0 * (60.0 / this.bpmVal)).toLong()
     }
 
     fun onProgressChanged(view: View) {
@@ -28,4 +76,6 @@ class Metronome : AppCompatActivity() {
         croller.progress = croller.progress + 1
         onProgressChanged(view)
     }
+
+
 }
