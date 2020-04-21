21fa703834a4542d49f2e6524c1be88b19c02d37
Luke Walsh <lwa383@uclive.ac.nz>
2020-04-21 19:54:10 +1200

    Added main activity animations

--- app/src/main/java/com/example/drumTutor/MainActivity.kt
+++ app/src/main/java/com/example/drumTutor/MainActivity.kt
@@ -5,14 +5,21 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runAnimation()
+
         val btnOpenMetronomeActivity: Button = findViewById(R.id.metronome_btn)
         val btnOpenRudimentsHome: Button = findViewById(R.id.rudiment_btn)
         btnOpenMetronomeActivity.setOnClickListener {
@@ -26,6 +33,25 @@ class MainActivity : Activity() {
         }
         persistDefaultSharedPreferences()
         readPersistedNumber()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        runAnimation()
+    }
+
+    private fun runAnimation() {
+        val ttb: Animation = AnimationUtils.loadAnimation(this, R.anim.ttb)
+        val ttb2: Animation = AnimationUtils.loadAnimation(this, R.anim.ttb2)
+        val stb: Animation = AnimationUtils.loadAnimation(this, R.anim.stb)
+
+        val logo: ImageView = findViewById(R.id.logoImage)
+        val rudimentBtn: TextView = findViewById(R.id.rudiment_btn)
+        val metronomeBtn: TextView = findViewById(R.id.metronome_btn)
+
+        logo.startAnimation(stb)
+        rudimentBtn.startAnimation(ttb)
+        metronomeBtn.startAnimation(ttb2)
     }
 
     /**
